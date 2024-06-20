3328eaccccbe983e661899e2a99b03b6d111c5ad
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-20 17:21:16 -0600

    quitando el paremtro mobal y solo agregamos el mobal

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,16 @@ const $mobal = document.querySelector('.mobal')
 const $btnUpdateProfile = document.querySelector('.profile__buton-update-profile')
 const $btnCloseModal = document.querySelector('.mobal__icon-close')
 
-const openMobal = (mobalEspicify = $mobal, className = '') => {
+const openMobal = (className = '', content) => {
   if (typeof className !== String || className === '') {
     returns
   }
-  mobalEspicify.classList.add(className ? className : 'active')
+  $mobal.classList.add(className ? className : 'active')
 }
 
-const closeMobal = (mobalEspicify = $mobal, className = '') => {
+const closeMobal = (className = '', content) => {
   if (typeof className !== String) {
     return
   }
-  mobalEspicify.classList.add(className ? className : 'active')
+  $mobal.classList.add(className ? className : 'active')
 }
