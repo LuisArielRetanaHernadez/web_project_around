449f760a9b54a438cb25a04c4ca97d0470b00819
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-20 17:43:23 -0600

    agregando el evento click al boton para actulizar el perfil

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,32 @@ const $mobal = document.querySelector('.mobal')
 const $btnUpdateProfile = document.querySelector('.profile__buton-update-profile')
 const $btnCloseModal = document.querySelector('.mobal__icon-close')
 
-const openMobal = (className = '', content) => {
+const openMobal = (className = '', content, title) => {
   if (typeof className !== String || className === '') {
     returns
   }
+  $mobal.querySelector('.mobal__title').textContent = title
+  $mobal.querySelector('.mobal__content').innerHTML = content
   $mobal.classList.add(className ? className : 'active')
 }
 
-const closeMobal = (className = '', content) => {
+const closeMobal = (className = '') => {
   if (typeof className !== String) {
     return
   }
   $mobal.classList.add(className ? className : 'active')
 }
+
+$btnUpdateProfile.addEventListener('click', () => {
+  const templateContentMobal = `
+          <form class="mobal__form" action="">
+            <input class="mobal__form-input" type="text" />
+            <input class="mobal__form-input" type="text" />
+            <button class="button mobal__button-submit" type="submit">
+              Guardar
+            </button>
+          </form>
+  `
+
+  openMobal(_, templateContentMobal, 'Editar Perfil')
+})
