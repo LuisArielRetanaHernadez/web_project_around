f7c7aaff508c51f131bc6143749cb3b1adf7d4b9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-06-20 15:07:14 -0600

    accediendo al mobal con la clase mobal y creando las clase openMobal y closeMobal

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const $mobal = document.querySelector('.mobal')
+
+const openMobal = () => {
+  $mobal.classList.add('active')
+}
+
+const closeMobal = () => {
+  $mobal.classList.remove('active')
+}
