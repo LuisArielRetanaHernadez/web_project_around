7ffe2342ccc31c8b3148a121a1eb2ca0a053045a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-26 22:36:16 -0600

    refacturado los formularios agregar photo y actualizar profile

--- scripts/script.js
+++ scripts/script.js
@@ -49,10 +49,13 @@ const mobalFormAddCard = () => {
   const formAddCard = `
           <form class="form mobal__form" data-form-select="true" id="form-add-card" action="">
             <fieldset class="form__field-set mobal__form-field-set" data-field-select>
+
               <input class="form__input mobal__form-input" id="input-profile-name" required minlength="7" maxlength="15" type=text" placeholder='title' />
               <span class="form__error-message" id="input-profile-name-error"></span>
+              
               <input class="form__input mobal__form-input" id="input-profile-url-image" type="url" placeholder='url' />
               <span class="form__error-message" id="input-profile-url-image-error"></span>
+              
               <button class="button form__button-submit mobal__button-submit" id="button-update-profile" type=submit">
                 Guardar
               </button>
@@ -67,10 +70,13 @@ const mobalUpadteProfile = () => {
   const formUpdate = `
           <form class="form mobal__form" id="form-update-profile" action="">
             <fieldset class="form__field-set" data-field-select>
-              <input class="form__input mobal__form-input" id="input-profile-name" type=text" value='${profileName.textContent}'/>
+              
+            <input class="form__input mobal__form-input" id="input-profile-name" type=text" value='${profileName.textContent}'/>
               <span class="form__error-message" id="input-profile-name-error"></span>
+              
               <input class="form__input mobal__form-input" id="input-profile-state" type="text" value='${profileState.textContent}'/>
               <span class="form__error-message" id="input-profile-state-error"></span>
+              
               <button class="button form__button-submit mobal__button-submit" id="button-update-profile" type=submit">
                 Guardar
               </button>
