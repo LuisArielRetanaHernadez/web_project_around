75ea6a6d49205fc04dfab16dd9a51e8b3fd2c33a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-27 15:43:50 -0600

    obteniendo componente mensaje de error del input corresponiente

--- scripts/script.js
+++ scripts/script.js
@@ -142,13 +142,13 @@ const generetePhoto = (title, url) => {
 }
 
 const showErrorInput = (formElement, inputForm, errorMessage) => {
-  const errorElementMessage = formElement.querySelector(`.${inputForm.id}-error`)
+  const errorElementMessage = formElement.querySelector(`#${inputForm.id}-error`)
   inputForm.classList.add('form__input_type_error')
   errorElementMessage.textContent = errorMessage
   errorElementMessage.classList.add('form__input-error_active')
 }
 const hiddeErrorInput = (formElement, inputForm) => {
-  const errorElementMessage = formElement.querySelector(`.${inputForm.id}-error`)
+  const errorElementMessage = formElement.querySelector(`#${inputForm.id}-error`)
   inputForm.classList.remove('form__input_type_error')
   errorElementMessage.classList.remove('form__input-error_active')
   errorElementMessage.textContent = ''
@@ -170,6 +170,7 @@ const hasInvalidInput = (inputs) => {
 }
 
 const toggleButtonState = (inputs, buttonSubmit) => {
+  console.log('has valid ', hasInvalidInput(inputs))
   if (hasInvalidInput(inputs)) {
     buttonSubmit.classList.add('form__button-submit_disabled')
     buttonSubmit.setAttribute('disabled', true)
@@ -181,6 +182,7 @@ const toggleButtonState = (inputs, buttonSubmit) => {
 
 const setEventListeners = formElement => {
   const inputs = Array.from(formElement.querySelectorAll('.form__input'))
+  console.log('inputs ', inputs)
   const buttonSubmit = formElement.querySelector('.form__button-submit')
   toggleButtonState(inputs, buttonSubmit)
   inputs.forEach((input) => {
@@ -199,8 +201,9 @@ const enableValidation = () => {
     })
     const formsSelect = Array.from(formElement.querySelectorAll('[data-field-select]'))
     console.log('formsSelect ', formsSelect)
-    formsSelect.forEach((formSelect) => {
-      setEventListeners(formSelect)
+    formsSelect.forEach((elemetForm) => {
+      console.log('formSelect ', elemetForm)
+      setEventListeners(elemetForm)
     })
   })
 }
