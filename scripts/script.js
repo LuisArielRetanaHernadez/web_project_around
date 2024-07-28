4fa68273119e267ec3e4254f68ee45226268d36b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-07-27 20:49:10 -0600

    eliminando las variables donde se almacenaba el boton de eliminar photo y cerrar mobal

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
-const btnCloseModal = document.querySelector('.mobal__icon-close')
-
 const btnUpdateProfile = document.querySelector('.profile__button-update-profile')
 const btnModalAddCard = document.querySelector('.profile__button-add-target')
-const btnDeleteCard = document.querySelectorAll('.photo__button-delete')
 
 const profileName = document.querySelector('.profile__name')
 const profileState = document.querySelector('.profile__state')
