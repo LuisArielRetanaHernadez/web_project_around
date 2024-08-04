a9402ef7e9d2a6c091fd944f11063d7d8bfac6dd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-03 19:08:11 -0600

    inicalizando los propidades de la clase

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,5 +1,11 @@
 class FormValidator {
   constructor(setting, formElement) {
-
+    this._formSelector = setting.formSelector;
+    this._inputSelector = setting.inputSelector;
+    this._submitButtonSelector = setting.submitButtonSelector;
+    this._inactiveButtonClass = setting.inactiveButtonClass;
+    this._inputErrorClass = setting.inputErrorClass;
+    this._errorClass = setting.errorClass;
+    this._formElement = formElement;
   }
 }
