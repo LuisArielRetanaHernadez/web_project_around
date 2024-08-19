e784f8015a6785d0d1ed3220a850c808a95812cb
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-19 14:35:49 -0600

    creando la clase PopupWithImage que se extiende de la clase Popup

---
+++ components/PopupWithImage.js
@@ -0,0 +1,10 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupImage = this._popup.querySelector('.popup__image');
+    this._popupCaption = this._popup.querySelector('.popup__caption');
+  }
+
+}
