abfe6e92117bbe2df8392c7c4bcdb2db00d1e12d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-19 14:04:43 -0600

    creando el metodo publico close para cerrar el popup

--- components/Popup.js
+++ components/Popup.js
@@ -5,6 +5,9 @@ export default class Popup {
 
   open() {
     this._popup.classList.add("popup_opened");
-    // document.addEventListener("keydown", this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove("popup_opened");
   }
 }
