7138e5b5538245ac245cd0cb01d60e759f70d91d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-31 14:45:25 -0600

    eliminando todas las variables de selectores

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,16 +1,4 @@
-const templateMobal = document.querySelector('#template-mobal')
-const templatePoppa = document.querySelector('#template-poppa')
 
-const btnUpdateProfile = document.querySelector('.profile__button-update-profile')
-const btnModalAddCard = document.querySelector('.profile__button-add-target')
-
-const profileName = document.querySelector('.profile__name')
-const profileState = document.querySelector('.profile__state')
-
-const cards = document.querySelector('.elements__photos')
-
-let mobal = null
-let poppa = null
 
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape') {
