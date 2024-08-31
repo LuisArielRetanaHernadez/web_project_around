72a2f91eb33d7ade8c984335873a6002f3d4c1f4
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-31 15:09:03 -0600

    creando la variable con el selector container de cards

--- constats/constats.js
+++ constats/constats.js
@@ -1,2 +1,3 @@
 export const buttonNewCard = document.querySelector('.profile__button-add-target')
 export const buttonEditProfile = document.querySelector('.profile__button-update-profile')
+export const containerCards = document.querySelector('.elements__card')
