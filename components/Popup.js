9230201091e28a16ee93bb15e7dfc041d195a4a7
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-31 19:13:52 -0600

    convertiendo el metodo setEventListeners en publico

--- components/Popup.js
+++ components/Popup.js
@@ -17,7 +17,7 @@ export default class Popup {
     }
   };
 
-  _setEventListeners() {
+  setEventListeners() {
 
     this._popup.addEventListener("click", (evt) => {
       if (evt.target === this._popup) {
