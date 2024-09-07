f3e88a44df14ac2f31cfe44ac680056d4f7f77ab
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-07 14:51:22 -0600

    agregando la configuracion para trabajar con postcss

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, { loader: "css-loader" }]
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          "postcss-loader"
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
