548ef48f5a0ad1a591d795ae795e35751b50ffcf
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-07 13:00:08 -0600

    quitando las demas opciones en la implementacion del plugin donde solo dejamos el tamplate

--- webpack.config.js
+++ webpack.config.js
@@ -33,9 +33,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebapckPlugin({
-      template: "./pages/index.html",
-      filename: "index.html",
-      inject: "body"
+      template: "./index.html",
     })
   ]
 }
