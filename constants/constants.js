ed283195ed4b1892ceaa12d84dd6a14e712ad3c9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-26 19:35:50 -0600

    creando las variables TOKEN y GroupId

--- constants/constants.js
+++ constants/constants.js
@@ -3,3 +3,5 @@ export const buttonEditProfile = document.querySelector('.profile__button-update
 export const containerCards = document.querySelector('.elements__card')
 export const profileName = document.querySelector('.profile__name')
 export const profileState = document.querySelector('.profile__state')
+export const TOKEN = 'd7d7e0e3-7d7d-4d7d-7d7d-7d7d7d7d7d7d'
+export const GroupId = 'XXXXXXX'
