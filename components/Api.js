cda72a2bc8fd9a89cabbe94cba480b4124a6b1db
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-26 20:17:13 -0600

    creando el metodo deleteCard

--- components/Api.js
+++ components/Api.js
@@ -35,4 +35,17 @@ export default class Api {
         return Promise.reject(`Error: ${res.status}`);
       })
   }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+  }
 }
