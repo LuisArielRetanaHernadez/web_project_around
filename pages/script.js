de32e3ff41790ec6ec62f762eefc4e9a890d8549
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-26 20:08:17 -0600

    importando la clase UserInfo

--- pages/script.js
+++ pages/script.js
@@ -4,6 +4,7 @@ import PopupWithForm from "../components/PopupWithForm.js"
 import Section from "../components/Section.js"
 import Card from "../components/Card.js"
 import FormValidator from "../components/FormValidator.js"
+import UserInfo from "../components/UserInfo.js"
 
 // style
 import './index.css'
