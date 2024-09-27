7a331788e30993e944e97e7a1c0550f729ce8073
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-26 20:15:44 -0600

    creando el metodo para crear una Card

--- components/Api.js
+++ components/Api.js
@@ -18,4 +18,21 @@ export default class Api {
         return Promise.reject(`Error: ${res.status}`);
       })
   }
+
+  createCard(name, link) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+  }
 }
