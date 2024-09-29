a2994a36a0e8dcb0aa7e89684aad60acd1f0b40b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-28 19:02:03 -0600

    cargando las cards desde la api

--- pages/script.js
+++ pages/script.js
@@ -30,36 +30,44 @@ const api = new Api({
 })
 
 
-const initialCards = [
-  {
-    name: 'fragmento de codigo html',
-    link: 'https://images.pexels.com/photos/270404/pexels-photo-270404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  },
-  {
-    name: 'carro mercede benz',
-    link: 'https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  },
-  {
-    name: 'manoplaza red bull',
-    link: 'https://images.pexels.com/photos/19417092/pexels-photo-19417092/free-photo-of-deportes-de-motor-exposicion-formula-1-f1.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  },
-  {
-    name: 'palacio de bellas artes desde arriba',
-    link: 'https://images.pexels.com/photos/1589347/pexels-photo-1589347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  },
-  {
-    name: 'angel de la independencia',
-    link: 'https://images.pexels.com/photos/14071000/pexels-photo-14071000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  },
-  {
-    name: 'perro chihuahua banandose',
-    link: 'https://images.pexels.com/photos/485294/pexels-photo-485294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+// const initialCards = [
+//   {
+//     name: 'fragmento de codigo html',
+//     link: 'https://images.pexels.com/photos/270404/pexels-photo-270404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+//   },
+//   {
+//     name: 'carro mercede benz',
+//     link: 'https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+//   },
+//   {
+//     name: 'manoplaza red bull',
+//     link: 'https://images.pexels.com/photos/19417092/pexels-photo-19417092/free-photo-of-deportes-de-motor-exposicion-formula-1-f1.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+//   },
+//   {
+//     name: 'palacio de bellas artes desde arriba',
+//     link: 'https://images.pexels.com/photos/1589347/pexels-photo-1589347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+//   },
+//   {
+//     name: 'angel de la independencia',
+//     link: 'https://images.pexels.com/photos/14071000/pexels-photo-14071000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+//   },
+//   {
+//     name: 'perro chihuahua banandose',
+//     link: 'https://images.pexels.com/photos/485294/pexels-photo-485294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+//   }
+// ]
+
+const initialCards = async () => {
+  const cards = await api.getCards()
+  if (cards) {
+    return cards
   }
-]
+  return []
+}
 
 
 const cardsInitial = new Section({
-  items: initialCards,
+  items: initialCards(),
   renderer: (item) => {
 
     const newCard = new Card(
