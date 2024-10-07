5e9ba38e1f39c6e722a1dcbcca6bb4b789379f71
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-06 20:41:46 -0600

    quitando la funcion handel del contructor y la pasamos al metodo open y el id para ejecutar la funcion en el event click

--- components/PopupWithConfirm.js
+++ components/PopupWithConfirm.js
@@ -1,15 +1,20 @@
 import Popup from "./Popup";
 
 export default class PopupWithConfirm extends Popup {
-  constructor(popupSelector, handleConfirmt) {
+  constructor(popupSelector) {
     super(popupSelector);
-    this._handleFormSubmit = handleConfirmt;
+  }
+
+  open(id, handleConfirmt) {
+    this._handleConfirmt = handleConfirmt;
+    this._id = id;
+    super.open();
   }
 
   setEventListeners() {
     super.setEventListeners();
     this._popup.querySelector('.popup__button-confirm').addEventListener('click', () => {
-      this._handleFormSubmit();
+      this._handleConfirmt(this._id);
       this.close();
     })
   }
